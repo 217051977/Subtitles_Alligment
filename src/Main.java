5515225d6ceb83074d320e0895759627049fe8de
Bruno <leal.bruno997@gmail.com>
2021-02-14 23:57:46 +0000

    Add time done

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,29 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
-    private static SubtitlesManager subtitlesManager = new SubtitlesManager();
-    private static DirectoryManager directoryManager = new DirectoryManager();
-    private static String fileName = "";
+    private static final SubtitlesManager subtitlesManager = new SubtitlesManager();
+    private static final DirectoryManager directoryManager = new DirectoryManager();
 
     public static void main(String[] args) throws FileNotFoundException {
-        switch (args.length) {
-
-        }
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Set drive: ");
         String drive = scanner.nextLine();
         setDrive(drive);
+        System.out.print("Set path: ");
         String path = scanner.nextLine();
         setPath(path);
+        System.out.print("Set file name: ");
         String name = scanner.nextLine();
+        System.out.print("Set time to add ->\nHours: ");
+        int h = scanner.nextInt();
+        System.out.print("Minutes: ");
+        int m = scanner.nextInt();
+        System.out.print("Seconds: ");
+        int s = scanner.nextInt();
 
         subtitlesManager.getFile(drive, path, name);
-        System.out.println(subtitlesManager.UpdateFile());
+//        subtitlesManager.UpdateFile(h, m, s);
+        System.out.println(subtitlesManager.UpdateFile(h, m, s));
     }
 
     private static void setDrive(String drive) {
--- src/Managers/SubtitlesManager.java
+++ src/Managers/SubtitlesManager.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.StringReader;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SubtitlesManager {
@@ -14,23 +15,57 @@ public class SubtitlesManager {
         System.out.println("Existes: " + file.exists());
     }
 
-    public String UpdateFile() throws FileNotFoundException {
+    public String UpdateFile(int h, int m, int s) throws FileNotFoundException {
         String fileContent = "";
         Scanner scanner = new Scanner(file);
         if (scanner.hasNextLine()) {
             do {
-                fileContent += getTimeLine(scanner.nextLine());
+                fileContent += getTimeLine(scanner.nextLine(), h, m, s);
             } while (scanner.hasNextLine());
+        }
+        if (fileContent.equals("")) {
+            return "Time line not found!";
         }
         return fileContent;
     }
 
-    private String getTimeLine(String line) {
-        String[] lineParts = line.split(":");
-        if (lineParts.length > 3) {
-            return line + "\n";
+    private String getTimeLine(String line, int h, int m, int s) {
+        String[] lineParts = line.split(" --> ");
+        if (lineParts.length > 1) {
+            return getTimeComponents(lineParts, h, m, s) + "\n";
         } else {
             return "";
         }
+    }
+
+    private String getTimeComponents(String[] lineParts, int h, int m, int s) {
+        String[] timeS = lineParts[0].split(":");
+        String[] timeE = lineParts[1].split(":");
+        return addTime(timeS, 0,0, 0) + " --> " + addTime(timeE, h, m, s);
+    }
+
+    private String addTime(String[] time, int hour, int min, int sec) {
+        String[] sParts = time[2].split(",");
+        int[] s = correctTime(Integer.parseInt(sParts[0]) + sec);
+        int[] m = correctTime(Integer.parseInt(time[1]) + min + s[1]);
+        int h = Integer.parseInt(time[0]) + hour + m[1];
+        return h + ":" + m[0] + ":" + s[0] + "," + sParts[1];
+    }
+
+    private int[] correctTime(int time) {
+        int res = time;
+        int extra = 0;
+        boolean done = false;
+
+        while (!done) {
+            if (res > 60) {
+                extra++;
+                res -= 60;
+            } else {
+                done = true;
+            }
+        }
+        int[] ret = {res, extra};
+        return ret;
     }
 }
